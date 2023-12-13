9580ef237bec8f15a83d74f3465b062fc2a18f83
Javier Cruz Santiago <javiercruzsantiago@icloud.com>
2023-12-13 01:37:18 -0600

    [NEW] - Se agrega DeppendenciesFramework como submodulo y al xcworkspace

--- IntegrationProject/IntegrationProject/Controllers/Initial/InitialViewController.swift
+++ IntegrationProject/IntegrationProject/Controllers/Initial/InitialViewController.swift
@@ -6,10 +6,20 @@
 //
 
 import UIKit
+import DependenciesFramework
 
 class InitialViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            let controller = DFController()
+            self.show(controller, sender: nil)
+        }
     }
 }
